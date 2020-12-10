6ce7020b99c1ff588de250c1bed01172f51ac2a3
cuongloveyou <55919737+cuongloveyou@users.noreply.github.com>
2020-12-10 19:53:12 +0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import request
 
@@ -49,6 +50,13 @@ def viethoa_process():
 def home_process():
     return "hihihaha"
 
+@app.route('/hello') #whenever this webserver is called with <hostname:port>/hello then this section is called
+def hello(): #The subroutine name that handles the call
+	output = 'Hello World'
+	return output #Whatever is returned from this subroutine is what is returned to the requester and is shown on the browser page
+
 # Start Backend
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='6868')
+    port = int(os.environ.get('PORT',
+                              5000))  # The port to be listening to — hence, the URL must be <hostname>:<port>/ inorder to send the request to this program
+    app.run(host='0.0.0.0', port=port)  # Start listening
