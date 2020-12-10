9072157226e7ca1e2ee6109dc831a760a169f66e
cuongloveyou <55919737+cuongloveyou@users.noreply.github.com>
2020-12-10 19:33:32 +0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,13 +1,62 @@
-import os
-from flask import Flask, request
+from flask import Flask
+from flask_cors import CORS, cross_origin
+from flask import request
 
-app = Flask(__name__) #create an instance of the Flask library
+# Khởi tạo Flask Server Backend
+app = Flask(__name__)
 
-@app.route('/hello') #whenever this webserver is called with <hostname:port>/hello then this section is called
-def hello(): #The subroutine name that handles the call
-	output = 'Hello World'
-	return output #Whatever is returned from this subroutine is what is returned to the requester and is shown on the browser page
+# Apply Flask CORS
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
+# http://127.0.0.1/add
+# http://127.0.0.1/minus
+# http://127.0.0.1/multi
+# http://127.0.0.1/div
+
+
+@app.route('/add', methods=['POST', 'GET'] )
+@cross_origin(origin='*')
+def add_process():
+    a = int(request.args.get('sothunhat'))
+    b = int(request.args.get('sothuhai'))
+    kq = a + b
+    return 'Kết quả là: ' + str(kq)
+
+
+@app.route('/minus', methods=['POST','GET'] )
+@cross_origin(origin='*')
+def minus_process():
+    a = int(request.args.get('sothunhat'))
+    b = int(request.args.get('sothuhai'))
+    kq = a - b
+    return 'Kết quả là: ' + str(kq)
+
+
+@app.route('/multi', methods=['POST','GET'] )
+@cross_origin(origin='*')
+def multi_process():
+    return "Hàm nhân"
+
+
+@app.route('/div', methods=['POST','GET'] )
+@cross_origin(origin='*')
+def div_process():
+    return "Hàm chia"
+
+
+@app.route('/viethoa', methods=['POST'] )
+@cross_origin(origin='*')
+def viethoa_process():
+    s = request.form.get("chuoiinput")
+    #s = request.args.get("chuoiinput")
+    return s.upper()
+
+@app.route('/', methods=['POST','GET'] )
+@cross_origin(origin='*')
+def home_process():
+    return "hihihaha"
+
+# Start Backend
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000)) #The port to be listening to — hence, the URL must be <hostname>:<port>/ inorder to send the request to this program
-	app.run(host='0.0.0.0', port=port)  #Start listening
+    app.run(host='127.0.0.1', port='6868')
